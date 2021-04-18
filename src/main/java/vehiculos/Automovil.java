198ec1fb5eb1ecf4cc1c3b3d7c60f944a36294f5
daruizg <79728404+daruizg@users.noreply.github.com>
2021-04-17 19:12:46 -0500

    1.0.1

--- src/main/java/vehiculos/Automovil.java
+++ src/main/java/vehiculos/Automovil.java
@@ -17,6 +17,7 @@ public class Automovil extends Vehiculo {
 		automovilesCreados ++;
 		
 		fabricante.getPais().setNumeroVentas(fabricante.getPais().getNumeroVentas() + 1);
+		fabricante.setNumVentas(fabricante.getNumVentas() + 1);
 	}
 	
 	// GETTERS Y SETTERS
--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -1,20 +1,36 @@
 package vehiculos;
 
+import java.util.ArrayList;
+
 public class Fabricante {
 	
 	// ATRIBUTOS
 	private String nombre;
 	private Pais pais;
+	private int numVentas = 0;
+	public static ArrayList<Fabricante> fabricas = new ArrayList<>();
 	
 	// CONSTRUCTOR
 	public Fabricante(String nombre, Pais pais) {
 		this.nombre = nombre;
 		this.pais = pais;
+		fabricas.add(this);
 	}
 	
 	// MÉTODOS
 	public static Fabricante fabricaMayorVentas() {
-		return new Fabricante("Renault", new Pais("Ecuador"));
+		
+		int mayorVenta 			   = fabricas.get(0).numVentas;
+		Fabricante mayorFabricante = fabricas.get(0);
+		
+		for (Fabricante fabricante: fabricas) {
+			
+			if (fabricante.numVentas > mayorVenta) {
+				mayorFabricante = fabricante;
+			}
+		}
+		
+		return mayorFabricante;
 	}
 	
 	// GETTERS Y SETTERS
@@ -32,5 +48,13 @@ public class Fabricante {
 
 	public void setPais(Pais pais) {
 		this.pais = pais;
+	}
+
+	public int getNumVentas() {
+		return numVentas;
+	}
+
+	public void setNumVentas(int numVentas) {
+		this.numVentas = numVentas;
 	}
 }
